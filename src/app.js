a4c0ef49a5a459ab7915e43ab31efd6610c5b3a5
M308126 <helitonvieira@sisconsultoria.com.br>
2024-05-28 10:22:35 -0300

    Criado rota para livro e objeto Livro no formato json e enviado via http método GET pegando os dados

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+      id: 1,
+      titulo: "O Senhor dos Anéis"
+    },
+    {
+      id: 2,
+      titulo: "O Hobbit"
+    }
+  ]
+
 app.get("/", (req,res)=>{
-    res.status(200).send("Curso de Node.js");
+    res.status(200).send("Curso de Node.js"); //utiliza send para enviar textos simples
 });
+
+//metodo GET pegar os dados
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros); // utiliza o json para objetos json
+  });
 
 export default app;
